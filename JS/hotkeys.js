a88cc29339fc686a63d29ac90333ddaaa0f5560b
Auracle <virgilfabre@outlook.com>
2022-05-28 20:50:47 -0400

    New hotkey modal

--- JS/hotkeys.js
+++ JS/hotkeys.js
@@ -1,60 +1,61 @@
 import ACTIONS from "./actions.js";
 
 const HOTKEYS = {
-	// TODO : IMPLEMENT
+	// TODO : Work In Progress Tools
 	COLOR_PICKER: {
 		WIP: true,
-		name: "Color picker",
+		tip: "Color picker",
 		key: "a",
 		func: () => ACTIONS.COLOR_PICKER(),
 	},
 	COLOR_SELECT: {
 		WIP: true,
-		name: "Color select",
+		tip: "Color select",
 		key: "c",
 		func: () => ACTIONS.COLOR_SELECT(),
 	},
 	CRAYON: {
 		WIP: true,
-		name: "Crayon",
+		tip: "Crayon",
 		key: "q",
 		func: () => ACTIONS.CRAYON(),
 	},
 	SAVE_ART: {
 		WIP: true,
-		name: "Save art",
+		tip: "Save art",
 		key: "s",
+		// TODO : Make specials function on the keyboard modal display
 		specials: ["ctrl"],
 		func: () => ACTIONS.SAVE_ART(),
 	},
 
 	OPEN_FILES: {
-		name: "Open files",
+		tip: "Open files",
 		key: "o",
 		func: () => ACTIONS.OPEN_FILES(),
 	},
 	NEW_ART: {
-		name: "New art",
+		tip: "New art",
 		key: "n",
 		func: () => ACTIONS.ART_NEW(),
 	},
 	HOTKEYS_MODAL: {
-		name: "Hotkeys",
+		tip: "Hotkeys",
 		key: "h",
 		func: () => ACTIONS.OPEN_MODAL("hotkeys"),
 	},
 	DESIRES_MODAL: {
-		name: "Desires",
+		tip: "Desires",
 		key: "d",
 		func: () => ACTIONS.OPEN_MODAL("desires"),
 	},
 	NEW_LAYER: {
-		name: "New layer",
+		tip: "New layer",
 		key: "l",
 		func: () => ACTIONS.NEW_LAYER(),
 	},
 	DELETE_LAYER: {
-		name: "Delete layer",
+		tip: "Delete layer",
 		key: "s",
 		func: () => ACTIONS.DELETE_LAYER(),
 	},
@@ -78,26 +79,57 @@ const hotkeys = (ev) => {
 	}
 };
 
+const keyboard_keys = [
+	[
+		"esc",
+		"f1",
+		"f2",
+		"f3",
+		"f4",
+		"f5",
+		"f6",
+		"f7",
+		"f8",
+		"f9",
+		"f10",
+		"f11",
+		"f12",
+	],
+	["~", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "back"],
+	["tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\"],
+	["caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "enter"],
+	["shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "shift"],
+	["ctrl", "win", "alt", "space", "alt", "ctrl"],
+];
+
 const init_modal = () => {
-	const modal = document.getElementById("hotkeys");
-	for (const HOTKEY in HOTKEYS) {
-		const row = document.createElement("div");
-		const text = document.createElement("div");
-		const keys = document.createElement("div");
-		row.className = "row";
-		text.classList = "text";
-		keys.className = "keys";
-		text.innerHTML = HOTKEYS[HOTKEY].name;
-		if (HOTKEYS[HOTKEY].WIP) text.innerHTML += " ⚡ UNDER CREATION";
-		const LIST = (HOTKEYS[HOTKEY].specials || []).concat(HOTKEYS[HOTKEY].key);
-		for (const KEY of LIST) {
-			const hotkey = document.createElement("div");
-			hotkey.className = "key";
-			hotkey.innerText = KEY;
-			keys.append(hotkey);
+	const keyboard = document.getElementById("keyboard");
+	for (const row of keyboard_keys) {
+		const rowDiv = document.createElement("div");
+		rowDiv.classList.add("row");
+		for (const key of row) {
+			const keyDiv = document.createElement("div");
+			keyDiv.classList.add("key");
+			keyDiv.textContent = key;
+			for (const HOTKEY in HOTKEYS) {
+				if (HOTKEYS[HOTKEY].specials) continue;
+				if (HOTKEYS[HOTKEY].key == key) {
+					const tip = document.createElement("div");
+					keyDiv.classList.add("used");
+					tip.classList.add("tip");
+					if (HOTKEYS[HOTKEY].WIP) {
+						tip.textContent = `WIP ⚡ ${HOTKEYS[HOTKEY].tip}`;
+						keyDiv.classList.add("wip");
+					} else tip.textContent = HOTKEYS[HOTKEY].tip;
+					keyDiv.append(tip);
+				}
+			}
+			if (key == ("back" || "tab" || "enter" || "caps" || "shift" || "ctrl"))
+				keyDiv.classList.add("large");
+			else if (key == "space") keyDiv.classList.add("space");
+			rowDiv.append(keyDiv);
 		}
-		row.append(text, keys);
-		modal.append(row);
+		keyboard.append(rowDiv);
 	}
 };
 
